19d92ea301aafdfe174c86f66f5ffe2abe25039e
Riivo Kolka <riivo.kolka@gmail.com>
2024-06-25 01:56:36 +0300

    variants

--- UDL-samples/color_codes.sql
+++ UDL-samples/color_codes.sql
@@ -32,9 +32,55 @@ Base3	"FDF6E3"	253	246	227	brwhite
 Yellow	"B58900"	181	137	0	yellow accent tones 'string' 
 Orange	"CB4B16"	203	75	22	brred
 Red	"DC322F"	220	50	47	red
-Magenta	"D33682"	211	54	130	magenta
-Violet	"6C71C4"	108	113	196	brmagenta INT32
+Magenta	"D33682"	211	54	130	magenta INT32
+Violet	"6C71C4"	108	113	196	brmagenta 
 Blue	"268BD2"	38	139	210	blue
 Cyan	"2AA198"	42	161	152	cyan SELECT
 Green	"859900"	133	153	0	green
 
+SELENIZED
+Color        CIE L*a*b*   HSB           sRGB      AppleRGB
+----------   ----------   -----------   -------   --------
+bg_0          8   0   0    35   0   9   #181818   #121212
+bg_1         15   0   0    35   0  15   #252525   #1C1C1C
+bg_2         25   0   0    35   0  23   #3B3B3B   #2D2D2D
+dim_0        50   0   0    35   0  47   #777777   #636363
+fg_0         75   0   0    35   0  72   #B9B9B9   #AAAAAA
+fg_1         88   0   0    35   0  87   #DEDEDE   #D6D6D6
+
+red          56  63  40     1  70  93   #ED4A46   #E13136
+green        67 -38  55    92  72  71   #70B433   #64A81D
+yellow       75   6  68    46  79  86   #DBB32D   #D1A416
+blue         56   0 -57   212  77  92   #368AEB   #2D76E9
+magenta      64  55 -15   325  53  92   #EB6EB7   #DE54AB
+cyan         72 -40  -4   174  68  77   #3FC5B7   #40BBA8
+orange       64  37  50    22  71  90   #E67F43   #DA6930
+violet       60  30 -45   263  43  88   #A580E2   #9169DD
+
+br_red       63  63  40     3  67 100   #FF5E56   #FB4343
+br_green     74 -38  55    92  65  78   #83C746   #77BD2D
+br_yellow    82   6  68    45  73  94   #EFC541   #E9B928
+br_blue      63   0 -57   214  69 100   #4F9CFE   #4289FF
+br_magenta   71  55 -15   325  49 100   #FF81CA   #F767C0
+br_cyan      79 -40  -4   173  60  85   #56D8C9   #53D2BD
+br_orange    71  37  50    22  67  98   #FA9153   #F37B3F
+br_violet    67  30 -45   263  41  96   #B891F5   #A67BF5
+
+OKSolar
+Color Name	OK Lightness	Chroma	Hue	Hex
+base03	27.4%	0.05	219.6	#002d38
+base02	32.1%	0.053	219.6	#093946
+base01	53.5%	0.029	219.6	#5b7279
+base00	54.4%	0.017	219.6	#657377
+base0	71.8%	0.017	198	#98a8a8
+base1	71.8%	0.03	198	#8faaab
+base2	93.4%	0.031	90	#f1e9d2
+base3	97.7%	0.012	90	#fbf7ef
+yellow	63.1%	0.129	86.4	#ac8300
+orange	63.1%	0.166	50.4	#d56500
+red	63.1%	0.221	21.6	#f23749
+magenta	63.1%	0.205	349.2	#dd459d
+violet	63.1%	0.121	280.8	#7d80d1
+blue	63.1%	0.141	244.8	#2b90d8
+cyan	63.1%	0.102	187.2	#259d94
+green	63.1%	0.148	118.8	#819500
